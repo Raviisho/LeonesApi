88ec9726791de1a00fb9883755071827fff9982b
Raviisho <marcoslescano02@gmail.com>
2023-03-01 19:21:01 -0300

    ApiUsuarios FIXED

--- Controllers/ApiUsuarios.cs
+++ Controllers/ApiUsuarios.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LeonesApi.Controllers
 {
-    [Route("api/{controller")]
+    [Route("api/[controller]")]
     [ApiController]
     public class ApiUsuarios : ControllerBase
     {
