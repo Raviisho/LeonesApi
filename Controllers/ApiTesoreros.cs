9b893a7017770f6a44c4f130eef2c314b8b6fe30
Raviisho <marcoslescano02@gmail.com>
2023-03-01 19:45:16 -0300

    ApiTesoreros DONE

---
+++ Controllers/ApiTesoreros.cs
@@ -0,0 +1,96 @@
+﻿using LeonesApi.Data;
+using LeonesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeonesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiTesoreros : ControllerBase
+    {
+        private readonly SmartsofMarcoslescanoContext _context;
+
+        public ApiTesoreros(SmartsofMarcoslescanoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tesorero>>> GetTesoreros()
+        {
+            return await _context.Tesoreros.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Tesorero>> GetTesoreros(int id)
+        {
+            var tesorero = await _context.Tesoreros.FindAsync(id);
+
+            if (tesorero == null)
+            {
+                return NotFound();
+            }
+
+            return tesorero;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTesorero(int id, Tesorero tesorero)
+        {
+            if (id != tesorero.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tesorero).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TesoreroExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Tesorero>> PostTesorero(Tesorero tesorero)
+        {
+            _context.Tesoreros.Add(tesorero);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTesorero", new { id = tesorero.Id }, tesorero);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTesorero(int id)
+        {
+            var tesorero = await _context.Tesoreros.FindAsync(id);
+            if (tesorero == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tesoreros.Remove(tesorero);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TesoreroExists(int id)
+        {
+            return _context.Tesoreros.Any(e => e.Id == id);
+        }
+    }
+}
