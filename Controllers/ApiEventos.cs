3e8a33b076321f30ac365bce71877a3f6f671b0a
Raviisho <marcoslescano02@gmail.com>
2023-03-02 23:26:57 -0300

    ApiEventos DONE

---
+++ Controllers/ApiEventos.cs
@@ -0,0 +1,96 @@
+﻿using LeonesApi.Data;
+using LeonesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeonesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiEventos : ControllerBase
+    {
+        private readonly SmartsofMarcoslescanoContext _context;
+
+        public ApiEventos(SmartsofMarcoslescanoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Evento>>> GetEventos()
+        {
+            return await _context.Eventos.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Evento>> GetEvento(int id)
+        {
+            var evento = await _context.Eventos.FindAsync(id);
+
+            if (evento == null)
+            {
+                return NotFound();
+            }
+
+            return evento;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEvento(int id, Evento evento)
+        {
+            if (id != evento.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(evento).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EventoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Evento>> PostEvento(Evento evento)
+        {
+            _context.Eventos.Add(evento);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEvento", new { id = evento.Id }, evento);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEvento(int id)
+        {
+            var evento = await _context.Eventos.FindAsync(id);
+            if (evento == null)
+            {
+                return NotFound();
+            }
+
+            _context.Eventos.Remove(evento);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EventoExists(int id)
+        {
+            return _context.Eventos.Any(e => e.Id == id);
+        }
+    }
+}
