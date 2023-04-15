61e4d53f5ec6c3216d35e724ccd1cbada700a044
Raviisho <marcoslescano02@gmail.com>
2023-04-14 21:21:49 -0300

    cuotas

--- Models/Cuota.cs
+++ Models/Cuota.cs
@@ -1,27 +1,28 @@
 ﻿using LeonesApi.Enums;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LeonesApi.Models;
 
 public partial class Cuota
 {
+    [Key]
     public int Id { get; set; }
-
+    [Required]
     public MesEnum Mes { get; set; }
-
+    [Required]
     public int Año { get; set; }
-
+    [Required]
+    [Precision(12, 2)]
     public decimal Monto { get; set; }
-
     public bool Cobrada { get; set; }
-
     public int SocioId { get; set; }
     [ForeignKey("SocioId")]
-    public Socio Socio { get; set; } = null!;
-
+    public Socio Socio { get; set; }
     public int TesoreroId { get; set; }
     [ForeignKey("TesoreroId")]
-    public Tesorero? Tesorero { get; set; }
+    public Tesorero Tesorero { get; set; }
 }
